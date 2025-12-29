b3d85dc6600c8b7e52dfdb159b334b429b41960d
sahilzinzuvadiya <sahilzinzuvadiya77@gmail.com>
2025-12-29 17:51:03 +0530

    Fourth

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -22,11 +22,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    if (!process.env.URL) {
+    if (!process.env.Mongodb_URI) {
       throw new Error("URL not found in environment variables");
     }
 
-    await mongoose.connect(process.env.URL, {
+    await mongoose.connect(process.env.Mongodb_URI, {
       serverSelectionTimeoutMS: 10000
     });
 
